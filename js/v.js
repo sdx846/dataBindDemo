76a40b312422051a94cea2276ea236c128b3e4cb
sdx846 <sdx_liuhui@163.com>
2020-08-18 18:01:09 +0800

    gengg

--- js/v.js
+++ js/v.js
@@ -1,38 +1,61 @@
 class Vue {
     constructor(options) {
-        const el = document.querySelector(options.el);
-        const data = options.data;
-        Object.keys(data).forEach(key => {
-            //可以替换一次。如果{{data}}第二次出现，则无法成功。
-            // el.innerHTML = el.innerHTML.replace(`{{${key}}}`,data[key]);
-            //优化一：使用正则表达式替换，html中所有的{{data}}可以被替换为正确的值。
-            el.innerHTML = el.innerHTML.replace(new RegExp(`{{${key}}}`, "g"), data[key]);
-        });
+        this.el = document.querySelector(options.el);
+        this.data = {};
+        this.init(options.data);
     };
+    async init(data) {
+        await this.responseData(data);
+        await this.replaceTemplateStrings();
+    }
+    replaceTemplateStrings() {
+        const stack = [this.el];
+        while (stack.length) {
+            const n = stack.pop();
+            if (n.childNodes.length) {
+                stack.push(...n.childNodes);
+            }
+            //纯文本节点
+            if (n.nodeType === Node.TEXT_NODE) {
+                this.replaceText(n);
+            }
+            //表单节点
+            if (n.nodeName === 'INPUT' && n.hasAttribute('v-model')) {
+                this.replaceModel(n);
+            }
+        }
+    }
+    //纯文本节点的{{}}绑定数据
+    replaceText(n) {
+        //读取文本，根据文本替换{{}}，而不是根据js中的数据数目，减少循环次数。
+        let keys = n.textContent.match(/(?<={{).*?(?=}})/g);
+        if (keys.length) {
+            keys = Array.from(new Set(keys));//去重，减少下面循环次数
+            keys.forEach(key => {
+                n.textContent = n.textContent.replace(
+                    new RegExp(`{{${key}}}`, 'g'),
+                    this.data[key]
+                );
+            });
+        }
+    }
+    //表单节点的v-model绑定数据
+    replaceModel(n) {
+        n.setAttribute('value', this.data[n.getAttribute('v-model')]);
+    }
+    //数据可响应
+    responseData(obj) {
+        Object.keys(obj).forEach(key => {
+            Object.defineProperty(obj, key, {
+                enumerable: true,
+                configurable: true,
+                get: function () {
+                   return this.data;
+                },
+                set: function (newVal) {
+                   this.data[key]=newVal;
+                }
+            });
+        });
+    }
 }
-//优化二：innerHTML 开销很大，因为值会被解析为 HTML，所以我们应该使用 textContent 或 innerText。但是将 innerHTML 替换为 innerText 或 textContent后标记变得更加复杂就很快不够用了。
-//一种解决方法是遍历 DOM，找到所有文本节点，然后替换文本。
-// class Vue {
-//     constructor(options) {
-//         this.el = document.querySelector(options.el);
-//         this.data = options.data;
-//         this.replaceTemplateStrings();
-//     };
-//     replaceTemplateStrings(){
-//         const stack=[this.el];
-//         while (stack.length){
-//             const n=stack.pop();
-//             if(n.childNodes.length){
-//                 stack.push(...n.childNodes);
-//             }
-//             if(n.nodeType===Node.TEXT_NODE){
-//                 Object.keys(this.data).forEach(key=>{
-//                     n.textContent=n.textContent.replace(
-//                         new RegExp(`{{${key}}}`),
-//                         this.data[key]
-//                     );
-//                 });
-//             }
-//         }
-//     }
-// }
