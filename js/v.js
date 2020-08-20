3bd11a32f0f87145aecc79b53476b6cbef595a5e
sdx846 <sdx_liuhui@163.com>
2020-08-20 18:01:05 +0800

    更改

--- js/v.js
+++ js/v.js
@@ -1,15 +1,68 @@
 class Vue {
     constructor(options) {
         this.el = document.querySelector(options.el);
-        this.data = {};
-        this.init(options.data);
+        this.data = options.data;
+        this.watchers = [];
+        //用于管理watcher的Dep对象
+        this.dep = {
+            add: (watcher) => {
+                this.watchers.push(watcher)
+            },
+            notify: (newValue) => {
+                this.watchers.forEach(function (fn) {
+                    fn(newValue)
+                })
+            }
+        };
+        this.init();
     };
-    async init(data) {
-        await this.responseData(data);
-        await this.replaceTemplateStrings();
+    init() {
+        let that = this;
+        that.replaceTemplateStrings();
+        that.initData();
     }
+
+    //初始化数据
+    initData() {
+        let that = this;
+        //将解析出来的watcher存入Dep中待用
+        this.dep.add(this.renderInput);
+        // that.dep.add(that.replaceTemplateStrings);
+
+        Object.keys(that.data).forEach(key => {
+            that.observer(that.data, key, that.data[key])
+        });
+    }
+    //数据可响应
+    observer(vm, key, value) {
+        let that = this;
+        Object.defineProperty(vm, key, {
+            enumerable: true,
+            configurable: true,
+            get: function () {
+                console.log('Get');
+                return value
+            },
+            set: function (newValue) {
+                if (value !== newValue) {
+                    value = newValue;
+                    console.log('Update')
+                    //将变动通知给相关的订阅者
+                    that.dep.notify(newValue);
+                    // that.replaceTemplateStrings();
+                }
+            }
+        })
+    }
+
+    // 模拟compile,通过对Html的解析生成一系列订阅者（watcher）
+    renderInput(newValue) {
+        console.log(newValue)
+    }
+    //根据不同的节点做不同的数据绑定
     replaceTemplateStrings() {
-        const stack = [this.el];
+        let that = this;
+        const stack = [that.el];
         while (stack.length) {
             const n = stack.pop();
             if (n.childNodes.length) {
@@ -17,11 +70,11 @@ class Vue {
             }
             //纯文本节点
             if (n.nodeType === Node.TEXT_NODE) {
-                this.replaceText(n);
+                that.replaceText(n);
             }
             //表单节点
             if (n.nodeName === 'INPUT' && n.hasAttribute('v-model')) {
-                this.replaceModel(n);
+                that.replaceModel(n);
             }
         }
     }
@@ -29,7 +82,7 @@ class Vue {
     replaceText(n) {
         //读取文本，根据文本替换{{}}，而不是根据js中的数据数目，减少循环次数。
         let keys = n.textContent.match(/(?<={{).*?(?=}})/g);
-        if (keys.length) {
+        if (keys&&keys.length) {
             keys = Array.from(new Set(keys));//去重，减少下面循环次数
             keys.forEach(key => {
                 n.textContent = n.textContent.replace(
@@ -37,25 +90,19 @@ class Vue {
                     this.data[key]
                 );
             });
+        }else{
+            // n.textContent = n.textContent.replace(
+            //     new RegExp(`{{${key}}}`, 'g'),
+            //     this.data[key]
+            // );
         }
     }
     //表单节点的v-model绑定数据
     replaceModel(n) {
-        n.setAttribute('value', this.data[n.getAttribute('v-model')]);
-    }
-    //数据可响应
-    responseData(obj) {
-        Object.keys(obj).forEach(key => {
-            Object.defineProperty(obj, key, {
-                enumerable: true,
-                configurable: true,
-                get: function () {
-                   return this.data;
-                },
-                set: function (newVal) {
-                   this.data[key]=newVal;
-                }
-            });
+        let that = this;
+        n.setAttribute('value', that.data[n.getAttribute('v-model')]);
+        n.addEventListener('input', function (e) {
+            that.data[n.getAttribute('v-model')] = e.target.value;
         });
     }
 }
